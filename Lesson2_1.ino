ffb774c9ba3560e569f2a089f7a6efcadcea5834
Kevin Boland <kmboland@gmail.com>
2016-12-03 21:40:56 -0500

    Update Lesson2_1.ino

--- Lesson2_1.ino
+++ Lesson2_1.ino
@@ -1,3 +1,4 @@
+//ask the class how to get the light to blink.
 int redLED=9;
 int onTime=500;
 int offTime=500;
@@ -12,5 +13,8 @@ pinMode(redLED, OUTPUT);
 void loop() {
   // put your main code here, to run repeatedly:
 digitalWrite(redLED,HIGH);
+delay(onTime);
+digitalWrite(redLED,LOW);
+delay(offTime);
 
 }
