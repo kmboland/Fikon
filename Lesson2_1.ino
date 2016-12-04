f8e5323c2189c0fa93f225564d9cfe696c987186
Kevin Boland <kmboland@gmail.com>
2016-12-03 21:13:35 -0500

    Create Lesson2_1.ino

---
+++ Lesson2_1.ino
@@ -0,0 +1,16 @@
+int redLED=9;
+int onTime=500;
+int offTime=500;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(redLED, OUTPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(redLED,HIGH);
+
+}
