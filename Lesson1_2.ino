b9d29154db6ee6bae802bf98cf323b6f21788c1d
Kevin Boland <kmboland@gmail.com>
2016-12-03 19:36:28 -0500

    Update Lesson1_2.ino

--- Lesson1_2.ino
+++ Lesson1_2.ino
@@ -1,14 +1,20 @@
+int LEDPin=13;
+
+
 void setup() {
   // put your setup code here, to run once:
-pinMode(13,OUTPUT);
+pinMode(LEDPin,OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-digitalWrite (13, HIGH);
-digitalWrite (13, LOW);
+digitalWrite (LEDPin, HIGH);
+//Cannot tell the LED is on or off. Needs a delay parameter for both HIGH and LOW. 
+//delay (1000);
+digitalWrite (LEDPin, LOW);
+//delay(1000);
 
 
 }
